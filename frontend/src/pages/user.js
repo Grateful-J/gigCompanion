1a8e3f440b69fcae9ee6fe199597b42be31c3bbc
mrdj863 <107734167+mrdj863@users.noreply.github.com>
2024-05-04 15:23:28 -0700

    rough draft of user profile

--- frontend/src/pages/user.js
+++ frontend/src/pages/user.js
@@ -1,1 +1,12 @@
 import "/style.css";
+
+// Function to Load navbar
+function loadNavbar() {
+  fetch("navbar.html")
+    .then((response) => response.text())
+    .then((html) => {
+      document.getElementById("navbar-placeholder").innerHTML = html;
+    });
+}
+
+loadNavbar();
