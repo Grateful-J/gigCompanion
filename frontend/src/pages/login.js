727dd0b9ebc505d0cf66692ebe3e3e5f0318c8bb
mrdj863 <107734167+mrdj863@users.noreply.github.com>
2024-05-03 20:23:38 -0400

    rough draft of login page

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,1 +1,18 @@
 import "/style.css";
+
+// Function to Load navbar
+function loadNavbar() {
+  fetch("navbar.html")
+    .then((response) => response.text())
+    .then((html) => {
+      document.getElementById("navbar-placeholder").innerHTML = html;
+    });
+}
+
+loadNavbar();
+
+//event listenter for signup button
+document.getElementById("signup-btn").addEventListener("click", function () {
+  document.getElementById("login").classList.add("hidden");
+  document.getElementById("signup").classList.remove("hidden");
+});
