381de38d95fd4c58a6c60911a520baf7144a955a
mrdj863 <107734167+mrdj863@users.noreply.github.com>
2024-05-03 21:10:55 -0500

    add reset form  funciton

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,4 +1,6 @@
 import "/style.css";
+const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
+const url = apiBaseUrl + "/api/users";
 
 // Function to Load navbar
 function loadNavbar() {
@@ -11,8 +13,18 @@ function loadNavbar() {
 
 loadNavbar();
 
-//event listenter for signup button
+// Function to reset inputs
+function resetForm() {
+  document.getElementById("login-username").value = "";
+  document.getElementById("login-password").value = "";
+  document.getElementById("signup-username").value = "";
+  document.getElementById("signup-password").value = "";
+}
+
+//event listenter for signup button >> show signup form / hide login form
 document.getElementById("signup-btn").addEventListener("click", function () {
   document.getElementById("login").classList.add("hidden");
   document.getElementById("signup").classList.remove("hidden");
 });
+
+//TODO: add forgot password functionality
