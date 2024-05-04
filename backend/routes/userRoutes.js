dc5198cf2a3986b747521afa575fc56437e6eab7
mrdj863 <107734167+mrdj863@users.noreply.github.com>
2024-05-03 20:45:49 -0400

    init userutes.js

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const User = require("..models/users.model.js");
+
+//Get All Users
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
