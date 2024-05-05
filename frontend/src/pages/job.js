543e0df5ec70412447803ea1e1d5ed724c2eb798
mrdj863 <107734167+mrdj863@users.noreply.github.com>
2024-05-05 08:49:32 -0700

    change hover clr: edit & del btns

--- frontend/src/pages/job.js
+++ frontend/src/pages/job.js
@@ -45,8 +45,10 @@ function displayJobs(jobs) {
         <td class="border-b border-gray-200 p-2">${job.rate}</td>
         <td class="border-b border-gray-200 p-2">${job.isFreelance ? "Yes" : "No"}</td>
         <td class="border-b border-gray-200 p-2">${job.isLocal ? "Yes" : "No"}</td>
-        <td class="border-b border-gray-200 p-2"><button class="edit-btn" data-id="${job._id}">Edit</button></td>
-        <td class="border-b border-gray-200 p-2"><button class="delete-btn bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" data-id="${
+        <td class="border-b border-gray-200 p-2"><button class="edit-btn bg-blue-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full" data-id="${
+          job._id
+        }">Edit</button></td>
+        <td class="border-b border-gray-200 p-2"><button class="delete-btn bg-blue-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full" data-id="${
           job._id
         }">Delete</button></td>
       `;
