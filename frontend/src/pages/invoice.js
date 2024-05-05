ac6e7b70a06df70c8e7f1e66695b1f2bb0289b7e
mrdj863 <107734167+mrdj863@users.noreply.github.com>
2024-05-05 00:49:06 -0700

    invoice page semi working. need to add hours logic and additions

--- frontend/src/pages/invoice.js
+++ frontend/src/pages/invoice.js
@@ -1,39 +1,44 @@
 import "/style.css";
 const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
-const gAPIKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+const url = apiBaseUrl + "/api";
 import { loadNavbar } from "../components/navbar.js";
 loadNavbar();
 
-// Event listener for job dropdown
-document.getElementById("job-dropdown").addEventListener("change", function () {
-  const selectedJobId = this.value;
-  fetch(`api/jobs/${selectedJobId}`)
-    .then((response) => response.json())
-    .then((job) => {
-      document.getElementById("job-name").textContent = job.name;
-      document.getElementById("start-date").textContent = job.startDate;
-      document.getElementById("end-date").textContent = job.endDate;
-      document.getElementById("client").textContent = job.client;
-      document.getElementById("rate").textContent = job.rate;
-      document.getElementById("location").textContent = job.location;
-      document.getElementById("job-code").textContent = job.code;
-      document.getElementById("hours-st").textContent = job.stHours;
-    })
-    .catch((error) => console.error("Error loading job details:", error));
-});
-
 // Example of loading jobs into the dropdown
 const loadJobs = function () {
-  fetch("api/jobs?isInvoiced=false") // Your API endpoint to fetch jobs
+  fetch(url + "/jobs") // Your API endpoint to fetch jobs
     .then((response) => response.json())
     .then((jobs) => {
       const select = document.getElementById("job-dropdown");
       jobs.forEach((job) => {
         const option = document.createElement("option");
-        option.value = job.id;
-        option.textContent = job.name;
+        option.value = job._id;
+        option.textContent = job.jobName;
         select.appendChild(option);
       });
     })
     .catch((error) => console.error("Error loading jobs:", error));
 };
+
+loadJobs();
+
+// Event listener for job dropdown
+document.getElementById("job-dropdown").addEventListener("change", function () {
+  const selectedJobId = this.value;
+  let startDate = new Date();
+  let endDate = new Date();
+  console.log(`Selected job ID: ${selectedJobId}`);
+  fetch(`${url}/jobs/${selectedJobId}`)
+    .then((response) => response.json())
+    .then((job) => {
+      document.getElementById("job-name").textContent = job.jobName;
+      document.getElementById("start-date").textContent = job.startDate;
+      document.getElementById("end-date").textContent = job.endDate;
+      document.getElementById("client").textContent = job.client;
+      document.getElementById("rate").textContent = job.rate;
+      document.getElementById("location").textContent = job.location;
+      //document.getElementById("job-code").textContent = job.code || "N/A";
+      //document.getElementById("hours-st").textContent = job.stHours;
+    })
+    .catch((error) => console.error("Error loading job details:", error));
+});
