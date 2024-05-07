36e1f95932598916cf8ec68b44fc20692561b177
mrdj863 <107734167+mrdj863@users.noreply.github.com>
2024-05-06 18:54:59 -0700

    Job details column- basic start

--- frontend/src/pages/crewCall.js
+++ frontend/src/pages/crewCall.js
@@ -4,3 +4,17 @@ import { loadNavbar } from "../components/navbar.js";
 import { fetchAndPopulateJobs, populateJobsDropdown } from "../util/jobService.js";
 loadNavbar();
 fetchAndPopulateJobs();
+
+// Populate Job details with selected job
+function populateJobDetails(job) {
+    const jobName = document.getElementById("job-name");
+    const startDate = document.getElementById("start-date");
+    const endDate = document.getElementById("end-date");
+    const client = document.getElementById("client");
+    const rate = document.getElementById("rate");
+    const location = document.getElementById("location");
+    const jobCode = document.getElementById("job-code");
+    const hoursSt = document.getElementById("hours-st");
+    const hoursOt = document.getElementById("hours-ot");
+    const hoursDt = document.getElementById("hours-dt");
+    jobName.textContent = job.jobName;
