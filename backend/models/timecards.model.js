bf8680c1f80a2facdcd2851a88d483b388fae80a
mrdj863 <107734167+mrdj863@users.noreply.github.com>
2024-05-12 09:53:33 -0700

    added duration logic in middleware

--- backend/models/timecards.model.js
+++ backend/models/timecards.model.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 
+// Schema for showDayEntries
 const showDayEntries = new mongoose.Schema(
   {
     date: Date,
@@ -20,26 +21,32 @@ const showDayEntries = new mongoose.Schema(
   }
 );
 
+// Schema for Timecard
 const timecardSchema = new mongoose.Schema(
   {
-    showDayEntries: [showDayEntries],
-    clockIn: Date,
-    clockOut: Date,
+    showDayEntries: [showDayEntries], // Import showDayEntries from showDayEntries model
+    clockIn: Date, // Legacy field- used for task hour tracking
+    clockOut: Date, // Legacy field- used for task hour tracking
     description: String,
     totalHours: Number,
     totalMinutes: Number,
-    duration: String,
+    duration: String, // Legacy field- used for task hour tracking
     isSubmitted: { type: Boolean, default: false },
-    dateSubmited: Date,
-    submissionBatchID: String,
     jobID: { type: mongoose.Schema.Types.ObjectId, ref: "Job" }, // Import Job ID from job model
-    day,
   },
 
   {
     timestamps: true,
   }
 );
+
+function calculateDuration(clockIn, clockOut) {
+  const start = new Date(clockIn);
+  const end = new Date(clockOut);
+  const duration = Math.ceil((end - start) / (1000 * 60 * 60 * 24)) + 1;
+  console.log("Calculated Duration:", duration);
+  return duration;
+}
 
 const Timecard = mongoose.model("Timecard", timecardSchema);
 
