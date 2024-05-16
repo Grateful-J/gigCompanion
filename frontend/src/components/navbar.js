77d83a4f723cb8222a2407ab15bbf80c2db4235f
mrdj863 <107734167+mrdj863@users.noreply.github.com>
2024-05-15 21:59:23 -0700

    hamburger functional

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -3,7 +3,16 @@ function loadNavbar() {
     .then((response) => response.text())
     .then((html) => {
       document.getElementById("navbar-placeholder").innerHTML = html;
+      toggleNavbar();
     });
+}
+
+//function to chage navebar
+function toggleNavbar() {
+  document.getElementById("navbar-toggle").addEventListener("click", function () {
+    const menu = document.getElementById("mobile-menu");
+    menu.classList.toggle("hidden");
+  });
 }
 
 export { loadNavbar };
