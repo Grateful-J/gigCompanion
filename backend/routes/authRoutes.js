e1cef3bdd595ce51ec0f77b08d9fe6e312a31799
mrdj863 <107734167+mrdj863@users.noreply.github.com>
2024-05-20 15:46:29 -0700

    add lougout route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,2 +1,5 @@
 const express = require("express");
 const router = express.Router();
+const { register, login, update, deleteUser, adminAuth, userAuth, logout } = require("../utils/auth");
+
+router.get("/logout", logout);
