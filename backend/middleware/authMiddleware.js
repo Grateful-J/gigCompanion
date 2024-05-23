49ad6070a1b3852e141cc4c659adc838fa0b0a65
mrdj863 <107734167+mrdj863@users.noreply.github.com>
2024-05-22 22:19:42 -0700

    add new(old) auth middleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -2,9 +2,49 @@ const jwt = require("jsonwebtoken");
 const jwtSecret = process.env.JWT_SECRET;
 const User = require("../models/users.model");
 
+exports.adminAuth = (req, res, next) => {
+  const token = req.cookies.jwt;
+  if (token) {
+    jwt.verify(token, jwtSecret, (err, decodedToken) => {
+      if (err) {
+        return res.status(401).json({ message: "Not authorized" });
+      } else {
+        if (decodedToken.role !== "admin") {
+          return res.status(401).json({ message: "Not authorized" });
+        } else {
+          next();
+        }
+      }
+    });
+  } else {
+    return res.status(401).json({ message: "Not authorized, token not available" });
+  }
+};
+exports.userAuth = (req, res, next) => {
+  const token = req.cookies.jwt;
+  if (token) {
+    jwt.verify(token, jwtSecret, (err, decodedToken) => {
+      if (err) {
+        return res.status(401).json({ message: "Not authorized" });
+      } else {
+        if (decodedToken.role !== "Basic") {
+          return res.status(401).json({ message: "Not authorized" });
+        } else {
+          next();
+        }
+      }
+    });
+  } else {
+    return res.status(401).json({ message: "Not authorized, token not available" });
+  }
+};
+
+//!--------------- OLD AUTH MIDDLEWARE ----------------- ! \\
+
 const requireAuth = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
+    console.log("requireAuth token", token);
     const decodedData = jwt.verify(token, jwtSecret);
     req.userDecodedData = decodedData;
 
