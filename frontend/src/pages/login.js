0a2cc859854652d21bcd7e032fee791e62615e88
mrdj863 <107734167+mrdj863@users.noreply.github.com>
2024-05-27 18:16:01 -0700

    login succesful but token was not stored.

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -26,7 +26,17 @@ document.getElementById("login-form").addEventListener("submit", async function 
     if (response.ok) {
       const data = await response.json();
       document.getElementById("loginMessage").innerText = `Login successful! User ID: ${data.userId}`;
+      console.log(data);
       sessionStorage.setItem("accessToken", data.accessToken);
+      sessionStorage.setItem("userId", data.userId);
+      sessionStorage.setItem("username", data.username);
+      sessionStorage.setItem("test", "test");
+
+      // Stores information about user in session storage
+
+      // stores access token in HTTP cookie
+      document.cookie = `accessToken=${data.accessToken};`;
+      window.location.href = "/home";
     } else {
       const errorMessage = await response.text();
       document.getElementById("loginMessage").innerText = `Login failed: ${errorMessage}`;
